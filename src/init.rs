e19031a4b151b8aec887c56b721bf9823980fd35
Josh Lynn <lynnjm7@gmail.com>
2017-06-04 00:53:33 -0700

    Added cmd line args and init processing

---
+++ src/init.rs
@@ -0,0 +1,39 @@
+extern crate git2; 
+
+use self::git2::Repository;
+
+
+use std::fs; 
+use std::env;
+use std::path::PathBuf;
+
+
+fn create_licensify_dir(path: &PathBuf) {
+    println!("Creating '~/.licensify' directory...");
+    match fs::create_dir(path) {
+        Ok(_) => println!("    licensify directory created!"),
+        Err(_) => println!("    unable to create licensify direcctory!"),
+    };
+}
+
+fn clone_licenses(path: &PathBuf) {
+    println!("Clonging license templates...");
+    let url = "https://github.com/lynnjm7/licenses.git";
+    
+    match Repository::clone(url, path) {
+        Ok(_) => println!("    license templates have been cloned!"),
+        Err(_) => println!("    we were unable to clone the license templates!"),
+    }
+}
+
+pub fn setup_licensify() {
+    let mut home_dir = env::home_dir().unwrap(); 
+    
+    // Create the home directory (for storing configuration files and the license snippets)
+    home_dir.push(".licensify");
+    create_licensify_dir(&home_dir); 
+    
+    // Clone the license templates into the "license" directory within the home directory
+    home_dir.push("license");
+    clone_licenses(&home_dir);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+extern crate licensify;
+extern crate clap;
+
+use clap::{App, Arg};
+use licensify::{init, license};
+
+
+fn setup_cmd_args<'a, 'b>() -> clap::App<'a, 'b> {
+    App::new("Licensify")
+        .version("1.0")
+        .author("Josh Lynn <lynnjm7@gmail.com")
+        .about("Creates a license file for a project")
+        .arg(Arg::with_name("init")
+                 .short("i")
+                 .long("init")
+                 .help("Runs the initial setup command"))
+        .arg(Arg::with_name("list")
+                 .short("l")
+                 .long("list")
+                 .help("List the available licenses"))
+        .arg(Arg::with_name("license").long("license").index(1))
+
+}
+
 fn main() {
+    let cmd_args = setup_cmd_args().get_matches();
+    if cmd_args.is_present("init") {
+        init::setup_licensify();
+        return;
+    }
+
+    if cmd_args.is_present("list") {
+        println!("list available licenses");
+        return;
+    }
+
+    let license = cmd_args.value_of("license").unwrap();
+    println!("{}", license);
+
+    // Setup command line arguments
+    //
+    // Process command line arguments
+    // if init: run initial setup functions
+    //
+    // if list: list possible options
+    //
+    // Read config from file and set values accordingly (not command line args will overwrite this)
+    //
+    // Given the license to produce, and the values to be used in the template, process the
+    // template, producing the final license output.
+
     println!("Hello, world!");
 }
